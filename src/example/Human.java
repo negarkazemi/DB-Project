006c03f887839214ecb53bdbde76f142fb1ef334
Negar <negarkazemi38@gmail.com>
2025-04-02 00:01:16 +0330

    Add getEntityCode methode.

--- src/example/Human.java
+++ src/example/Human.java
@@ -5,6 +5,13 @@ import db.*;
 public class Human extends Entity {
     public String name;
     public int age;
+    public static final int HUMAN_ENTITY_CODE = 14;
+
+
+    @Override
+    public int getEntityCode() {
+        return HUMAN_ENTITY_CODE;
+    }
 
     public Human(String name, int age) {
         this.name = name;
